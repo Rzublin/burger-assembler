060cf8db1a608baa69258956379227b04470e03a
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-06-15 20:57:07 +0200

    refactor: update layout component path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "./components/layout/Layout";
+import Layout from "./components/Layout/Layout";
 import "./App.css";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
