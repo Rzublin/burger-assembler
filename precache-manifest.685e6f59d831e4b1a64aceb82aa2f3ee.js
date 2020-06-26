fdb77ecaad57daf5926ae5a113561e3115203c6e
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-06-26 11:20:00 +0200

    Updates

---
+++ precache-manifest.685e6f59d831e4b1a64aceb82aa2f3ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5edd65cc9c7b6a6b32261b6171b16ab",
+    "url": "/burger-assembler/index.html"
+  },
+  {
+    "revision": "78236207156537aadcdc",
+    "url": "/burger-assembler/static/css/main.442a36f1.chunk.css"
+  },
+  {
+    "revision": "c6548b6ca2e78ac04770",
+    "url": "/burger-assembler/static/js/2.253d0bcc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger-assembler/static/js/2.253d0bcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78236207156537aadcdc",
+    "url": "/burger-assembler/static/js/main.2382b2e9.chunk.js"
+  },
+  {
+    "revision": "3dbf2f4bec43edc90e39",
+    "url": "/burger-assembler/static/js/runtime-main.bc64d2e9.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-assembler/static/media/burger-logo.b8503d26.png"
+  }
+]);
